934cd96d69649de9dda4f985ebf12f8774aa7f56
jiwonAn1 <jwan0131@gmail.com>
2022-09-20 16:15:20 +0900

    Initial commit

---
+++ src/main/java/jiwonan/study/climbing_back/ClimbingBackApplication.java
@@ -0,0 +1,13 @@
+package jiwonan.study.climbing_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jiwonan/study/climbing_back/ClimbingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwonan.study.climbing_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
